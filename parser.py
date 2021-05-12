ea488e14ed8b746cd33dc0cef8af32d0c15ad539
Rahul Bansal <32533369+RahulBansal1998@users.noreply.github.com>
2021-05-13 01:46:13 +0530

    bug resolved

--- parser.py
+++ parser.py
@@ -1,7 +1,6 @@
 import gem_parser
 import argparse
 import os
-import time
 # def get_argument():
 #     parser = argparse.ArgumentParser()
 #     parser.add_argument("-f", "--file",required=True,help = "Filename",type=str) 
@@ -19,8 +18,12 @@ import time
 
 from flask import Flask,render_template,request,jsonify
 from werkzeug.utils import secure_filename
+import time
  
 app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(),'resume')
+print (app.config['UPLOAD_FOLDER'])
 
 @app.route('/')
 def form():
@@ -30,13 +33,11 @@ def form():
 def upload():
     if request.method == 'POST':
         file = request.files['file']
-        print(file.filename)
-        file.save(secure_filename(file.filename))
-        filename = os.getcwd()
-        filename = filename + '/' + file.filename
-        print (filename)
-        time.sleep(5)
-        return jsonify(gem_parser.resume_data(filename))
+        print(file)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'],secure_filename(file.filename)))
+        print (os.path.join(app.config['UPLOAD_FOLDER'],secure_filename(file.filename)))
+        time.sleep(3)
+        return jsonify(gem_parser.resume_data(os.path.join(app.config['UPLOAD_FOLDER'],secure_filename(file.filename))))
  
 
 if __name__ == '__main__':
